f38c1fd2dabae3f4c7ac68f3826adb168c3d3143
ramdhanyA <ariqathallah38@gmail.com>
2023-10-06 08:23:35 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 740093 on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
